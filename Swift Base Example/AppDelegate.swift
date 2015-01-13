3c982b99ed66a89aad128e8b87e7c85e9937ad07
Ethan Mick <ethanmski@gmail.com>
2015-01-13 13:55:14 -0500

    Removed API key and added an automatic insert.

--- Swift Base Example/AppDelegate.swift
+++ Swift Base Example/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         let credentials = CMAPICredentials.sharedInstance() as CMAPICredentials!
-        credentials.appIdentifier = "c231a674044045af994fa0256007e1cc"
-        credentials.apiKey = "9d94c776b4834cd6ba0085da71113a68"
+        credentials.appIdentifier = "<#App ID#>"
+        credentials.apiKey = "<#API Key#>"
         
         return true
     }
